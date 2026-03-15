7159c9eae4c9e53cd32f0b3adc589e7332f0a0f7
Dumbo4nonimo <144817276+Dumbo4nonimo@users.noreply.github.com>
2026-03-15 13:55:47 -0500

    Documentation
    Added predicates for validating clauses and formulas in CNF. Improved parsing and un-parsing functions for BNF representation.

--- Taller2RacketCode.rkt
+++ Taller2RacketCode.rkt
@@ -57,8 +57,12 @@ lista-clausulas)))
             [else (cons (car cl) (clausula->varbools (cdr cl)))])))
 
 
-;; Predicados - estructuras de listas
+;; PREDICADOS (estructuras de listas)
 
+;; Verifica si una estructura corresponde a una cláusula válida del AST
+;; donde la clausula es una disyuncion de literales (OR).
+
+;; clausula? : Exp → Boolean
 (define es-clausula? (lambda (cl)
           (cond
             [(not (list? cl)) #f]
@@ -67,6 +71,11 @@ lista-clausulas)))
             [(and (number? (car cl)) (equal? (cadr cl) 'or)) (es-clausula? (cddr cl))]
             [else #f])))
 
+
+;; Verifica si una estructura corresponde a una lista válida de cláusulas en el AST
+;; donde las cláusulas estan conectadas por conjunción (AND).
+
+;; clausulas? : Exp → Boolean
 (define es-clausulas? (lambda (cls)
            (cond
              [(null? cls) #f]
@@ -74,72 +83,106 @@ lista-clausulas)))
              [(and (es-clausula? (car cls)) (equal? (cadr cls) 'and)) (es-clausulas? (cddr cls))]
              [else #f])))
 
+
+;; Predicado que verifica si una expresión tiene la estructura
+;; de una fórmula en Forma Normal Conjuntiva (FNC)
+
+;; es-fnc? : Exp -> Boolean
 (define es-fnc? (lambda (fnc-exp)
                   (and (list? fnc-exp) (>= (length fnc-exp) 3)
                        (equal? (car fnc-exp) 'FNC) (es-clausulas? (caddr fnc-exp)))
                   ))
 
 
-;; DataTypes - estructuras abstratas
+;; Implementación con DataTypes (estructuras abstratas)
 
+;; Tipo de dato que representa una clausula de la FNC.
+;; Una cláusula es una disyunción (OR) de literales.
 (define-datatype clausula clausula?
   (un-literal (lit number?))
   (or-literal (lit number?) (resto clausula?)))
 
+;; Tipo de dato que representa una lista de cláusulas
+;; conectadas mediante el operador lógico AND.
 (define-datatype clausulas clausulas?
   (una-clausula (cl clausula?))
   (and-clausulas (cl clausula?) (resto clausulas?)))
 
+;; Tipo de dato que representa una fórmula en
+;; Forma Normal Conjuntiva (FNC).
 (define-datatype fnc fnc?
   (fnc-exp (numvars number?) (cls clausulas?)))
 
 
-
 ;; PARSE
 
-;; Convierte una clausula BNF a lista de literales
-;; (1 or 2 or -3) -> (1 2 -3)
-
+;; Convierte una cláusula en BNF a su representación AST
+;; eliminando los operadores 'or'.
+;;
+;; Ejemplo:
+;; (parse-clausula '(1 or -2 or 3)) -> (1 -2 3)
+;;
+;; parse-clausula : ClausulaBNF -> ClausulaAST
 (define parse-clausula (lambda (cl)
     (cond
       [(null? cl) empty]
       [(number? (car cl)) (cons (car cl) (parse-clausula (cdr cl)))]
-      [(equal? (car cl) 'or) (parse-clausula (cdr cl))]
+      [(eqv? (car cl) 'or) (parse-clausula (cdr cl))]
       [else empty])))
 
 
-;; Convierte clausulas BNF
-;; ((1 or 2) and (3 or -1)) -> ((1 2) (3 -1))
-
-(define parse-clausulas(lambda (cls)
+;; Convierte una lista de clausulas en BNF a su representación AST
+;; eliminando el operador 'and'.
+;;
+;; Ejemplo:
+;; (parse-clausulas '((1 or 2) and (-3))) -> ((1 2) (-3))
+;;
+;; parse-clausulas : ClausulasBNF -> ClausulasAST
+(define parse-clausulas
+  (lambda (cls)
     (cond
       [(null? (cdr cls)) (list (parse-clausula (car cls)))]
       [else
-       (cons (parse-clausula (car cls)) (parse-clausulas (cddr cls)))])))
+       (cons (parse-clausula (car cls))
+             (parse-clausulas (cddr cls)))])))
 
 
-;; Convierte una FNC completa
-;; PARSEBNF ::= BNF -> AST
-
+;; Convierte una formula SAT en BNF a su arbol de sintaxis abstrata (AST)
+;;
+;; Ejemplo:
+;; (FNC 3 '((1 or 2) and (3))) -> (FNC 3 ((1 2) (3)))
+;;
+;; PARSEBNF : BNF -> AST
 (define PARSEBNF (lambda (fnc)
-    (list (car fnc) (cadr fnc) (parse-clausulas (caddr fnc)))))
-
-
+    (list (car fnc) (cadr fnc) (parse-clausulas (caddr fnc)))
+ ))
 
 
 ;; UNPARSE
 
-;; Convierte (1 2 -3) -> (1 or 2 or -3)
 
+;; Convierte una clausula del AST a su representacinn BNF
+;; insertando el operador 'or'.
+;;
+;; Ejemplo:
+;; (unparse-clausula '(-1 9 3)) -> (-1 or 9 or 3)
+;;
+;; unparse-clausula : ClausulaAST -> ClausulaBNF
 (define unparse-clausula (lambda (cl)
     (cond
       [(null? (cdr cl)) (list (car cl))]
       [else
-       (cons (car cl) (cons 'or (unparse-clausula (cdr cl))))])))
+       (cons (car cl) (cons 'or (unparse-clausula (cdr cl))))]
+     ))
+ )
 
-
-;; Convierte ((1 2) (3 -1)) -> ((1 or 2) and (3 or -1))
-
+;; Convierte una lista de clausulas AST en su representación BNF
+;; insertando el operador "and" entre cada clausula
+;;
+;; Ejemplo:
+;; (unparse-clausulas '((1 2) (3 -1))) -> ((1 or 2) and (3 or -1))
+;;
+;; unparse-clausulas : ClausulasAST -> ClausulasBNF
 (define unparse-clausulas (lambda (cls)
     (cond
       [(null? (cdr cls)) (list (unparse-clausula (car cls)))]
@@ -148,7 +191,12 @@ lista-clausulas)))
      )))
 
 
-;; Convierte AST basado en listas a BNF
-;; UNPARSEBNF ::= AST -> BNF
+;; Inversa de PARSEBNF, recibe un arbol de sintaxis abstracta (AST)
+;; y lo convierte nuevamente a la gramatica BNF
+;;
+;; Ejemplo:
+;; (UNPARSEBNF '(FNC 4 ((1 -2) (-3 4)))) -> (FNC 4 ((1 or -2) and (-3 or 4)))
+;;
+;; UNPARSEBNF : AST -> BNF
 (define UNPARSEBNF (lambda (lst)
     (list (car lst) (cadr lst) (unparse-clausulas (caddr lst)))))
